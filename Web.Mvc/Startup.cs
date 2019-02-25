e24b728afe7940ecc7c93e5e3039cd0b26a67d07
Laura Thomas <lthomas@edcoe.org>
2019-02-25 10:52:47 -0800

    Initial commit

---
+++ Web.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web.Mvc.Startup))]
+namespace Web.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
